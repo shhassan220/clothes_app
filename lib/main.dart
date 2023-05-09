fa6b865a9035c379346f696b23fc6e75357a5d6e
shhassan220 <shhassan435@gmail.com>
2023-05-09 03:01:13 -0700

    now complete app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Column(
+          children: [
+            Image.asset("assets/images/detail2.png")
+          ],
+        ),
+      ),
+    );
+  }
+}
